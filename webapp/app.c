40478107bc213d75d7b6c0ccc301b3d8e0e5aff7
PublicHadyniak <public.hadyniak@gmail.com>
2016-05-19 12:19:35 +0300

    app run with run options type and it's constructor

--- webapp/app.c
+++ webapp/app.c
@@ -1,6 +1,49 @@
+#include <stdlib.h>
+
 #include "app.h"
 
+// @todo move to separate module
+typedef struct list_s list_t;
+
 struct app_s {
+    list_t * routes;
 	/* smth */
 	void * userContext;
 };
+
+app_t * app_new(void) {
+    app_t * self = malloc(sizeof(struct app_s));
+    self->userContext = NULL;
+    return self;
+}
+
+void app_free(app_t * self) {
+    free(self);
+}
+
+void * app_setContext(app_t * self, void * ctx) {
+    void * oldContext = self->userContext;
+    self->userContext = ctx;
+    return oldContext;
+}
+
+void * app_getContext(app_t * self) {
+    return self->userContext;
+}
+
+void app_route(app_t * self, const char * uri, request_handler_fn handler, http_method_t methods) {
+    // @todo add data to routes
+
+}
+
+void app_run(app_t * self, app_run_options_t * opts) {
+    // @todo
+}
+
+app_run_options_t app_run_options_create(void) {
+    app_run_options_t opts = {
+        .ipAddress = APP_ADDR_DEFAULT,
+        .port = APP_PORT_DEFAULT
+    };
+    return opts;
+}
--- webapp/app.h
+++ webapp/app.h
@@ -4,11 +4,13 @@
 #include "response.h"
 #include "http.h"
 
+const int APP_PORT_DEFAULT = 5000;
+const char * const APP_ADDR_DEFAULT = "127.0.0.1";
 //
 // web server app type
 typedef struct app_s app_t;
 
-// 
+//
 // http request user handler function pointer
 typedef void (*request_handler_fn)(app_t * app, request_t * req, response_t * res);
 
@@ -21,8 +23,15 @@ void * app_getContext(app_t * self);
 //
 // set user handler function for uri request with defined methods
 void app_route(app_t * self, const char * uri, request_handler_fn handler, http_method_t methods);
+
+typedef struct app_run_options_s {
+    const char * ipAddress;
+    unsigned int port;
+} app_run_options_t;
+
+// create initialized instance of run options struct
+app_run_options_t app_run_options_create(void);
 //
-// default port (5000) on localhost
-void app_run(app_t * self); 
-void app_run_port(app_t * self, int port);
-void app_run_ip_port(app_t * self, const char * ipAddr, int port);
+// starts infinite loop with clients requests handling
+// opts - pass NULL for default port (5000) on localhost
+void app_run(app_t * self, app_run_options_t * opts);
